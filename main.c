16224f30bceca6ff49c6e3973f795429ad1309a7
Ashton Moore <ashton.chris.moore@gmail.com>
2021-02-08 17:23:12 +1300

    Added tokenisation of input

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 
 #define TSS_READ_LINE_BUFSIZE 1024
+#define TSS_TOK_BUFSIZE 64
+#define TSS_TOK_DELIM " \t\r\n\a"
+
+char **tss_split_line(char *line)
+{
+    int bufsize = TSS_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "TSS: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TSS_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += TSS_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "TSS: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        token = strtok(NULL, TSS_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
 
 char *tss_read_line(void)
 {
@@ -51,8 +85,8 @@ void tss_loop(void)
     do {
         printf("> ");
         line = tss_read_line();
-        // args = tss_split_line(line);
-        // status = tss_execute(args);
+        args = tss_split_line(line);
+        // status = tss_execute(args););
 
         free(line);
         free(args);
